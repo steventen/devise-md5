912ad6f6b7e98f5d0c80ec2271f177a16bfe90ea
Steven Yue <jincheker@gmail.com>
2014-02-12 16:32:31 -0500

    Added example.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseMd5::Application.config.secret_token = '1582f847f47acb97892968b6762da4f9aacf8bce78599aa040a9fee2626135e7080447ee30f85e127728f5ce3fc4fed270a8a77c6e2bb6738036632d224aa261'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseMd5::Application.config.session_store :cookie_store, key: '_devise-md5_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeviseMd5::Application.config.session_store :active_record_store
